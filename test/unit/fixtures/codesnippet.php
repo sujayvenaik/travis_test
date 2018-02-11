665b6a4898b28be077445f8e143b2f5057b28b9f
sujayvenaik <sujayvenaik@gmail.com>
2018-02-11 22:08:18 +0530

    pecl sort

--- test/unit/fixtures/codesnippet.php
+++ test/unit/fixtures/codesnippet.php
@@ -1,24 +1,18 @@
 <?php
-
 $client = new http\Client;
 $request = new http\Client\Request;
-
 $body = new http\Message\Body;
 $body->addForm(array(
-  '1' => 'ws',
-  's' => 'a',
-  'wq' => 'qqqq'
-), NULL);
+'fdjks' => 'dsf',
 
+), NULL);
+$request->setBody($body);
 $request->setRequestUrl('https://postman-echo.com/post');
 $request->setRequestMethod('POST');
-$request->setBody($body);
-
 $request->setHeaders(array(
-  
 ));
-
 $client->enqueue($request)->send();
 $response = $client->getResponse();
 
 echo $response->getBody();
+?>
