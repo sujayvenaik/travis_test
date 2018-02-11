1a98e37b089961094c2a148cf77b05e516f028a9
sujayvenaik <sujayvenaik@gmail.com>
2018-02-11 21:08:55 +0530

    form-data6

--- test/unit/fixtures/codesnippet.php
+++ test/unit/fixtures/codesnippet.php
@@ -1,23 +1,20 @@
 <?php
-
 $client = new http\Client;
-$request = new http\Client\Request;
-
+$request = new http\Client\Request();
 $body = new http\Message\Body;
-$body->addForm(array(
-  '1' => 'ws',
-  's' => 'a',
-  'wq' => 'qqqq'
-), NULL);
+$body->append(new http\QueryString(array(
+'1' => 'a',
+'2' => 'b',
+'c' => 'c',
+)));
+$request->setBody($body);
+$request->setRequestUrl('https://postman-echo.com/post');
+$request->setRequestMethod('POST');
 $request->setHeaders(array(
-    'Content-Type' => 'text/plain'
-  ));
-// $request->setBody($body);
-
-
-$request = new http\Client\Request('POST', 'https://postman-echo.com/post', NULL, $body);
-
+'Content-Type' => 'application/x-www-form-urlencoded',
+));
 $client->enqueue($request)->send();
 $response = $client->getResponse();
 
 echo $response->getBody();
+?>
