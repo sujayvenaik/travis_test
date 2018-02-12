cec95c15c0761886e606e636c258a40595ca968f
sujayvenaik <sujayvenaik@gmail.com>
2018-02-12 09:29:52 +0530

    http snippet1

--- test/unit/fixtures/codesnippet.php
+++ test/unit/fixtures/codesnippet.php
@@ -1,16 +1,9 @@
 <?php
 $request = new HttpRequest();
-$request->setUrl('https://postman-echo.com/post');
-$request->setMethod(HTTP_METH_POST);
+$request->setUrl('https://postman-echo.com/headers');
+$request->setMethod(HTTP_METH_GET);
 $request->setHeaders(array(
-    'Content-Type' => 'application/x-www-form-urlencoded',
-));
-$request->setContentType('application/x-www-form-urlencoded');
-$request->setPostFields(array(
-    '1' => 'a',
-    '2' => 'b',
-    'c' => 'c',
-
+    'my-sample-header' => 'Lorem ipsum dolor sit amet',
 ));
 
 try {
