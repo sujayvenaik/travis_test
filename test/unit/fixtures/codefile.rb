ab54cea946ecdf6fd575906b8d0b09e465fc870f
sujayvenaik <sujayvenaik@gmail.com>
2018-02-12 16:32:33 +0530

    change-RUBY

--- test/unit/fixtures/codefile.rb
+++ test/unit/fixtures/codefile.rb
@@ -6,8 +6,8 @@ http = Net::HTTP.new(url.host, url.port)
 
 request = Net::HTTP::Post.new(url)
 http.use_ssl = true
-request["Content-Type"] = 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+request["Content-Type"] = 'text/html'
 
-request.body = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"fdjks\"\r\n\r\ndsf\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"sdf\"\r\n\r\nnj\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+request.body = "<html>\n  Test Test\n</html>"
 response = http.request(request)
 puts response.read_body
