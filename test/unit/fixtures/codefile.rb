a30de268b4c0a9d084f16496b883bf934cd44f5d
sujayvenaik <sujayvenaik@gmail.com>
2018-02-12 14:48:31 +0530

    rubyyyyy

--- test/unit/fixtures/codefile.rb
+++ test/unit/fixtures/codefile.rb
@@ -4,7 +4,7 @@ require 'net/http'
 url = URI("https://postman-echo.com/post")
 
 http = Net::HTTP.new(url.host, url.port)
-
+http.use_ssl = (url.scheme == "https")
 request = Net::HTTP::Post.new(url)
 request["Content-Type"] = 'application/javascript'
 request.body = "var val = 6;\nconsole.log(val);"
