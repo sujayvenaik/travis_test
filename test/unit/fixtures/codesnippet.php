8c009a59de98c4e592a838bb96b64fc5524d9db7
sujayvenaik <sujayvenaik@gmail.com>
2018-02-12 10:46:34 +0530

    213456

--- test/unit/fixtures/codesnippet.php
+++ test/unit/fixtures/codesnippet.php
@@ -1,14 +1,18 @@
 <?php
+
 /*
  *  simple HttpRequest example using PHP
  *  tom slankard
  */
+
 class HttpRequest {
+
   public $url = null;
   public $method = 'GET';
   public $body = null;
   public $headers = Array();
   public $allow_redirect = true;
+
   private $url_info = null;
   private $host_name = null; 
   private $host_ip = null;
@@ -18,13 +22,17 @@ class HttpRequest {
   private $response_message = null;
   private $port = null;
   private $verbose = true;
+
   public function __construct($url, $method = 'GET') {
+
     $this->url = $url;
     $this->method = $method;
+
     //  parse url
     $this->url_info = parse_url($url);
     $this->host_name = $this->url_info['host'];
     $this->host_ip = gethostbyname($this->host_name);
+
     //  get port number given the scheme
     if(!isset($this->url_info['port'])) {
       if($this->url_info['scheme'] == "http")
@@ -34,14 +42,18 @@ class HttpRequest {
     } else {
       $this->port = $this->url_info['port'];
     }
+
     //  add default headers
     $this->headers["Host"] = "$this->host_name";
     $this->headers["Connection"] = "close";
+
   }
+
   private function constructRequest() {
     $path = "/";
     if(isset($this->url_info['path']))
       $path = $this->url_info['path'];
+
     $req = "$this->method $path HTTP/1.1\r\n";
     foreach($this->headers as $header => $value) {
       $req .= "$header: $value\r\n";
@@ -49,10 +61,12 @@ class HttpRequest {
   
     return "$req\r\n";
   }
+
   ///  reads a line from a file
   function readLine($fp)
   {
     $line = "";
+
     while (!feof($fp)) {
       $line .= fgets($fp, 2048);
       if (substr($line, -1) == "\n") {
@@ -61,18 +75,25 @@ class HttpRequest {
     }
     return $line;
   }
+
   public function send() {
+
     $fp = fsockopen($this->host_ip, $this->port);
+
     //  construct request
     $request = $this->constructRequest();
+
     //  write request to socket
     fwrite($fp, $request);
+
     //  read the status line
     $line = $this->readline($fp);
     $status = explode(" ", $line);
+
     //  make sure the HTTP version is valid
-    if(!isset($status[0]) || !preg_match("/^HTTP\/\d+\.?\d*/", $status[0]))
+    if(!isset($status[0]) || !preg_match("/^HTTP\/\d\.?\d*/", $status[0]))
       die("Couldn't get HTTP version from response.");
+
     //  get the response code
     if(!isset($status[1]))
       die("Couldn't get HTTP response code from response.");
@@ -82,14 +103,17 @@ class HttpRequest {
     if(!isset($status[2]))
       die("Couldn't get HTTP response reason from response.");
     else $this->response_reason = $status[2];
+
     //  read the headers
     do {
       $line = $this->readLine($fp);
       if($line != "") { 
         $header = split(":", $line);
+
         $this->response_headers[$header[0]] = ltrim($header[1]);
       }
     } while(!feof($fp) && $line != "");
+
     //  read the body
     $this->response_body = "";
     do {
@@ -98,22 +122,31 @@ class HttpRequest {
         $this->response_body .= "$line\n";
       }
     } while(!feof($fp));
+
     //  close the connection
     fclose($fp);
+
     return TRUE;
   }
+
   public function getStatus() {
     return $this->response_code;
   }
+
   public function getHeaders() {
     return $this->response_headers;
   }
+
   public function getResponseBody() {
     return $this->response_body;
   }
+
 }
+
+
 $req = new HttpRequest("http://www.iana.org/domains/example/", "GET");
 $req->headers["Connection"] = "close";
 $req->send() or die("Couldn't send!");
 echo( $req->getResponseBody() );
+
 ?>
