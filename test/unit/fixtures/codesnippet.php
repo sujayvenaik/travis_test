4736116d3108f526a6da3adccdf6f7703ead37b8
sujayvenaik <sujayvenaik@gmail.com>
2018-02-20 16:02:19 +0530

    testing pm and sujya

--- test/unit/fixtures/codesnippet.php
+++ test/unit/fixtures/codesnippet.php
@@ -1,6 +1,8 @@
 <?php
+
 $client = new http\Client;
 $request = new http\Client\Request;
+
 $body = new http\Message\Body;
 $body->append('Duis posuere augue vel cursus pharetra. In luctus a ex nec pretium. Praesent neque quam, tincidunt nec leo eget, rutrum vehicula magna.
 Maecenas consequat elementum elit, id semper sem tristique et. Integer pulvinar enim quis consectetur interdum volutpat.');
@@ -8,10 +10,12 @@ Maecenas consequat elementum elit, id semper sem tristique et. Integer pulvinar 
 $request->setRequestUrl('https://postman-echo.com/post');
 $request->setRequestMethod('OPTIONS');
 $request->setBody($body);
+
 $request->setHeaders(array(
-        'content-type' => 'text/plain'
+  'Content-Type' => 'text/plain'
 ));
+
 $client->enqueue($request)->send();
 $response = $client->getResponse();
+
 echo $response->getBody();
-?>
