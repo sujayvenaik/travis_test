6350ccd599a874eac8f6ce4c84b411f074b11f0a
sujayvenaik <sujayvenaik@gmail.com>
2018-02-20 10:45:25 +0530

    test

--- test/unit/fixtures/codesnippet.php
+++ test/unit/fixtures/codesnippet.php
@@ -2,7 +2,7 @@
 $client = new http\Client;
 $request = new http\Client\Request;
 $body = new http\Message\Body;
-$body->append("Duis posuere augue vel cursus pharetra. In luctus a ex nec pretium. Praesent neque quam, tincidunt nec leo eget, rutrum vehicula magna.\nMaecenas consequat elementum elit, id semper sem tristique et. Integer pulvinar enim quis consectetur interdum volutpat.");
+$body->append('Duis posuere augue vel cursus pharetra. In luctus a ex nec pretium. Praesent neque quam, tincidunt nec leo eget, rutrum vehicula magna.\nMaecenas consequat elementum elit, id semper sem tristique et. Integer pulvinar enim quis consectetur interdum volutpat.');
 $request->setRequestUrl('https://postman-echo.com/post');
 $request->setRequestMethod('OPTIONS');
 $request->setBody($body);
