e0488ee4656c6825113cadce7997394cb3409179
sujayvenaik <sujayvenaik@gmail.com>
2018-02-21 02:16:35 +0530

    test

--- test/unit/fixtures/code.php
+++ test/unit/fixtures/code.php
@@ -1,13 +1,17 @@
 <?php
+
 $client = new http\Client;
 $request = new http\Client\Request;
 
-$request->setRequestUrl('https://mockbin.org/request');
-$request->setRequestMethod('VIEW');
+$request->setRequestUrl('https://www.google.com');
+$request->setRequestMethod('GET');
 $request->setHeaders(array(
-        'Content-Type' => 'text/plain'
+  'TEST' => '\\"doublequotes\\"',
+  'testing' => ''singlequotes'',
+  'my-sample-header' => 'Lorem ipsum dolor sit amet'
 ));
+
 $client->enqueue($request)->send();
 $response = $client->getResponse();
+
 echo $response->getBody();
-?>
