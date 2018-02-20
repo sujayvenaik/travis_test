aadee1ee6dfff60fbb774d408e97bd1e9e7e060e
sujayvenaik <sujayvenaik@gmail.com>
2018-02-20 16:32:22 +0530

    TEST

--- test/unit/fixtures/test.php
+++ test/unit/fixtures/test.php
@@ -1,16 +1,21 @@
 <?php
+
 $client = new http\Client;
 $request = new http\Client\Request;
+
 $body = new http\Message\Body;
 $body->append('Duis posuere augue vel cursus pharetra. In luctus a ex nec pretium. Praesent neque quam, tincidunt nec leo eget, rutrum vehicula magna.
 Maecenas consequat elementum elit, id semper sem tristique et. Integer pulvinar enim quis consectetur interdum volutpat.');
+
 $request->setRequestUrl('https://mockbin.org/request');
 $request->setRequestMethod('LINK');
 $request->setBody($body);
+
 $request->setHeaders(array(
-        'content-type' => 'text/plain'
+  'Content-Type' => 'text/plain'
 ));
+
 $client->enqueue($request)->send();
 $response = $client->getResponse();
+
 echo $response->getBody();
-?>
