af4c9536204c4568d1d15f256869e326d849d446
sujayvenaik <sujayvenaik@gmail.com>
2018-02-23 18:34:13 +0530

    true trim

--- test/unit/initial.test.js
+++ test/unit/initial.test.js
@@ -133,7 +133,7 @@ describe('Curl converter', function () {
             convert.convert(request, {indentType: 'spapce', 
                 indentCount: 4,
                 requestTimeout: 100,
-                requestBodyTrim: true,
+                requestBodyTrim: false,
                 addCacheHeader: false,
                 followRedirect: true}, function (err, snippet) {
                 if (err) {
